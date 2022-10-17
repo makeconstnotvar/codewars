5db614e18aa2596e799d907518fb67d796cd27be
alexkot <makeconstnotvar@gmail.com>
2022-10-18 01:06:13 +0300

    задачки

--- Container-With-Most-Water.js
+++ Container-With-Most-Water.js
@@ -4,9 +4,21 @@ function maxArea(arr) {
   let j = arr.length - 1;
 
   while (i < j) {
-
+    const h = Math.min(arr[i], arr[j]);
+    const w = j - i;
+    const newVol = w * h;
+    if (newVol > vol) {
+      vol = newVol;
+    }
+    if (arr[i] < arr[j]) {
+      i++;
+    } else {
+      j--;
+    }
   }
+  return vol;
 }
 
-let result = maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])
+//let result = maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])
+let result = maxArea([2,3,4,5,18,17,6])
 console.log(result);
