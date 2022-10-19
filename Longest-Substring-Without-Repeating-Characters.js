ac80e9e07a392ebf641e0bd7b35b7b8941943d50
alexkot <makeconstnotvar@gmail.com>
2022-10-19 03:20:53 +0300

    задачки

--- Longest-Substring-Without-Repeating-Characters.js
+++ Longest-Substring-Without-Repeating-Characters.js
@@ -1,5 +1,31 @@
 function lengthOfLongestSubstring(str) {
+  let cache = {};
+  let currentLen = 0;
+  let maxLen = 0;
+  let from = 0;
+  let to = 0;
+  let scanFrom = 0;
+  let scanTo = 0;
+  for (let i = 0; i < str.length; i++) {
+    const letter = str[i]
+    if (!cache[letter]) {
+      cache[letter] = 1;
+      currentLen++;
+      scanTo=i;
+      if (currentLen > maxLen) {
+        maxLen = currentLen;
+        from = scanFrom;
+        to = i;
+      }
+    } else {
+      cache = {[letter]: 1};
+      currentLen = 1;
+      scanFrom = i;
+      scanTo = i;
+    }
 
+  }
+  return str.substring(from,to+1);
 }
 
 const result = lengthOfLongestSubstring('abcabcbb');
