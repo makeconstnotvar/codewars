f49746a1bdd1f84a26869ce9a01c7b8f483817db
Alex Kot <makeconstnotvar@protonmail.com>
2022-10-25 00:57:21 +0300

    задачки

--- Median-of-Two-Sorted-Arrays.js
+++ Median-of-Two-Sorted-Arrays.js
@@ -1,19 +1,28 @@
 function findMedianSortedArrays(nums1, nums2) {
+  let result = 0;
   /*
   [999] - 2 / 1
   [1234567] - 7 / 4
   9
   12345 5 67999
   */
-  let mid1 = Math.floor(nums1.length / 2)
-  let mid2 = Math.floor(nums2.length / 2)
+  let total = nums1.length + nums2.length;
+  let half = Math.floor(total / 2);
+  let mid1 = Math.floor(nums1.length / 2);
+  let mid2 = half - mid1; //Math.floor(nums2.length / 2);
+
+  if (nums1[mid1] <= nums2[mid2 + 1] && nums2[mid2] <= nums1[mid1 + 1]) {
+
+  }
+
+  return result;
 }
 
 function test(nums1, nums2) {
   let result
   let merged = nums1.concat(nums2).sort();
   let mid = Math.floor(merged.length / 2);
-  console.log('mid',mid)
+  console.log('mid', mid)
   if (merged.length % 2 == 0) {
     result = (merged[mid] + merged[mid - 1]) / 2
   } else {
@@ -23,4 +32,4 @@ function test(nums1, nums2) {
 }
 
 console.log(test([1, 7, 8], [2, 3, 4, 5, 6]))
-console.log(findMedianSortedArrays([1, 2, 3], [4, 5, 6, 7]))
+console.log(findMedianSortedArrays([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3, 4, 5]))
