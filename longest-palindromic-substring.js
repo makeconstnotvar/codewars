dafd5c958e381e6711348c049434bd4ed7b2c6cd
Alex Kot <makeconstnotvar@protonmail.com>
2023-08-28 01:57:59 +0300

    задачки

--- longest-palindromic-substring.js
+++ longest-palindromic-substring.js
@@ -1,17 +1,31 @@
-function longestPalindrome (s) {
-  let max = ''
+function longestPalindrome(s) {
+  let max = s.length == 1 ? s : s[1];
+
   for (let i = 1; i < s.length; i++) {
-    let left = i - 1;
-    let right = i + 1;
-    while (left > -1 && right < s.length) {
-      if (s[left] == s[right]) {
-        max = max.length > right - left + 1 ? max : s.substring(left, right+1   )
+    if (s[i-1] == s[i]) {
+      let left = i-1;
+      let right = i ;
+      while (left > -1 && right < s.length && s[left] == s[right]) {
+        if (right - left + 1 > max.length) {
+          max = s.substring(left, right + 1);
+        }
+        left--;
+        right++;
       }
-      left--;
-      right++;
+    } else if (s[i - 1] == s[i + 1]) {
+      let left = i - 1;
+      let right = i + 1;
+      while (left > -1 && right < s.length && s[left] == s[right]) {
+        if (right - left + 1 > max.length) {
+          max = s.substring(left, right + 1);
+        }
+        left--;
+        right++;
+      }
     }
+
   }
   return max;
 }
 
-console.log(longestPalindrome('cbbd'))
+console.log(longestPalindrome('bb'))
