d292bec2fed6109dca3d8d2a06d0d1a6e9a98bc6
Alex Kot <akotlyachkov@gmail.com>
2024-03-27 00:47:58 +0300

    задачки

--- house-robber.js
+++ house-robber.js
@@ -1,20 +1,29 @@
-const rob = (nums) => {
-  const dp = [
-    2,
-    7
-  ];
-  for (let i = 2; i < nums.length; i++) {
-    let max = Math.max(nums[i], nums[i + 1] || 0)
-      //dp[i] = dp[i - 2] + max;
-    dp[i] = max;
+const rob = nums => {
+
+  let a = 0;
+  let b = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let max = Math.max(a + nums[i], b);
+    a = b;
+    b = max;
   }
-  return dp;
-}
+  return b;
+};
 
-console.log(rob([2, 7, 9, 3, 1]))
-
-
+console.log(rob([2, 1, 1, 2]))
 /*
+  0 0 2 1 1 2
+  0 2 *
+    2 2 *
+      2 3 *
+        3 4 *
+
+
+
+      a b
+      1 3
+        a b
+
  i   0  1  2  3  4
  v   2  7  9  3  1
      -     *  *
